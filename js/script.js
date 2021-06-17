a1787ebb54f78c371db055da343df32acbbcd746
Roberto <robertoanfuso98@gmail.com>
2021-06-17 18:11:09 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//Milestone 1 Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
+//Tramite il forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
+
+const gatti=[
+    {
+        nome:"pamela",
+        colore:"",
+        eta:1.5,
+        gender:"female"
+    },
+    {
+        nome:"franco",
+        colore:"",
+        eta:1.5,
+        gender:"male"
+    },
+    {
+        nome:"yumi",
+        colore:"",
+        eta:1.5,
+        gender:"female"
+    },
+    {
+        nome:"maurizio",
+        colore:"",
+        eta:1.5,
+        gender:"male"
+    },
+    {
+        nome:"jessy",
+        colore:"",
+        eta:1.5,
+        gender:"female"
+    },
+    {
+        nome:"linobanfi",
+        colore:"",
+        eta:1.5,
+        gender:"male"
+    }
+];
+
